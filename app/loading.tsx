1facb8c80c6d0d8f7a66f7b897d642871ffb2a93
abhi23333 <161305604+abhi23333@users.noreply.github.com>
2026-08-27 14:32:54 +0530

    Update loading.tsx

--- app/loading.tsx
+++ app/loading.tsx
@@ -2,14 +2,19 @@ import Image from 'next/image'
 
 export default function Loading() {
   return (
-    <div className="loading-screen">
-      <div className="loading-logo">
+    <div className="fixed inset-0 z-[9999] flex items-center justify-center bg-background">
+      <div className="relative flex items-center justify-center">
+        {/* Loading ring */}
+        <div className="absolute h-[230px] w-[230px] animate-spin rounded-full border-[2px] border-foreground/10 border-t-gold" />
+
+        {/* Logo */}
         <Image
-          src="/icon.svg"
+          src="/images/kukkadapu-logo-v2.png"
           alt="Kukkadapu Associates"
-          width={180}
-          height={180}
+          width={300}
+          height={170}
           priority
+          className="relative h-auto w-[180px] object-contain"
         />
       </div>
     </div>
