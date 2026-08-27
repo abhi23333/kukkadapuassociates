0048e070c35e5f9951624888c02689c229d67f2a
abhi23333 <161305604+abhi23333@users.noreply.github.com>
2026-08-27 14:21:58 +0530

    Create loading.tsx

---
+++ app/loading.tsx
@@ -0,0 +1,17 @@
+import Image from 'next/image'
+
+export default function Loading() {
+  return (
+    <div className="loading-screen">
+      <div className="loading-logo">
+        <Image
+          src="/icon.png"
+          alt="Kukkadapu Associates"
+          width={180}
+          height={180}
+          priority
+        />
+      </div>
+    </div>
+  )
+}
